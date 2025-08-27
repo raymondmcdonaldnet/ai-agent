4c894579f7fe5546c3c05dc6a2d207b34fc4f640
R.L. McDonald <me@rlmcdonald.com>
2025-08-27 09:54:55 -0500

    Add verbose CLI option

--- main.py
+++ main.py
@@ -6,9 +6,14 @@ from google.genai import types
 
 
 def main():
-    if len(sys.argv) <= 1:
+    is_verbose = False
+
+    if len(sys.argv) <= 2:
         print("Usage: ai-agent [prompt string]")
         sys.exit(1)
+    elif len(sys.argv) == 3 and sys.argv[2] == "--verbose":
+        is_verbose = True
+
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -19,7 +24,9 @@ def main():
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
     response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+
     print(response.text)
+    print(f"User prompt: {user_prompt}")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
