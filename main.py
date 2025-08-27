b589454ae555fa285f94c0b7e3d81300790d6073
R.L. McDonald <me@rlmcdonald.com>
2025-08-27 09:42:00 -0500

    Store user prompt in a list
    Later we will use it to model an ongoing conversation with the LLM.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
@@ -13,10 +14,15 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=sys.argv[1])
+    user_prompt = sys.argv[1]
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
 
 
 if __name__ == "__main__":
