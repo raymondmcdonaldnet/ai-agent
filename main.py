3d6ef3a963ae09ff7717c15124005f22fb6f5433
R.L. McDonald <me@rlmcdonald.com>
2025-08-27 09:38:47 -0500

    Take a prompt from the command line

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
 
 def main():
+    if len(sys.argv) <= 1:
+        print("Usage: ai-agent [prompt string]")
+        sys.exit(1)
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    test_response = client.models.generate_content(model="gemini-2.0-flash-001",
-                                                   contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-    print(test_response.text)
-    print(f"Prompt tokens: {test_response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {test_response.usage_metadata.candidates_token_count}")
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=sys.argv[1])
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
